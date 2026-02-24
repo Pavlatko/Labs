b47ac29af9c27fe04cd9567078bbd0b45e9b275b
Pavlatko <pavlatko@seznam.cz>
2026-02-24 16:02:47 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+﻿// Service Worker pro PavlatkoLabs
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+  // Tato část umožňuje offline fungování, zatím ji necháme prázdnou
+  e.respondWith(fetch(e.request));
+});
